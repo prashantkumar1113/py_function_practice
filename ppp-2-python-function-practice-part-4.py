34fcbd1add1d9062b1754bc68443d80abed84df8
prashantkumar1113 <prashantkumar1113@gmail.com>
2022-06-22 09:23:05 -0400

    finished 4th function

--- ppp-2-python-function-practice-part-4.py
+++ ppp-2-python-function-practice-part-4.py
@@ -24,6 +24,14 @@ print(rev_string("Prashant"))
 # Write a Python function called num_within() to check whether a number falls in a given range.
 # The function accepts the number, beginning of range, and end of range (inclusive) as arguments.
 # Examples: num_within(3,2,4) returns True, num_within(3,1,3) returns True, num_within(10,2,5) returns False.
+def num_within(num, beginning, end):
+    return num >= beginning and num <= end 
+
+print(num_within(3,2,4))
+print(num_within(3,1,3))
+print(num_within(10,2,5))
+
 # Write a Python function called pascal() that prints out the first n rows of Pascal's triangle.
 # The function accepts the number n, the number of rows to print
-# Note : Pascal's triangle is an arithmetic and geometric figure first imagined by Blaise Pascal. Each number is the two numbers above it added together.
+# Note : Pascal's triangle is an arithmetic and geometric figure first imagined by Blaise Pascal. 
+# Each number is the two numbers above it added together.
